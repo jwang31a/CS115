645fa5df2fd969608ea4cded9f3e5a7de3d14f0f
Jun Hong Wang <junhongwang31@gmail.com>
2023-10-10 14:50:13 -0400

    adsf

--- 10-10/notes.py
+++ 10-10/notes.py
@@ -27,3 +27,41 @@ def isPalindrome(word):
 """
 tuples and dictionaries! yay
 """
+
+#tuple, this variable has all of these values
+#different from a list, immutable
+t = 1234, 4567, "hello"
+print(t)
+try:
+    t[1] = True
+except:
+    print("can't do that bozo, tuples are immutable")
+
+#tuples can be added
+u = t + (12, 34, 56)
+print(u)
+
+#tuples can also be embedded or in a list
+u = t, (12, 34, 56)
+print(u)
+
+#since the tuple is in a list and we're just replacing part of the list, it's possible
+#we're not actually modifying the tuple
+#but the moment that the items in the tuple are modified, error
+l1 = [ (23, 34), (56, 78) ]
+l1[0] = (45, 56)
+
+#if we try to modify an index of a tuple, we can't
+#but if we get into the list inside of the tuple, we can modify it
+t1 = ( [34, 45], [67, 78] )
+try:
+    t1[0] = [56, 78]
+except:
+    print("can't do that one buckaroo")
+t1[0][0] = 56
+
+empty = ()
+#tuple with a single item 
+#CAREFUL WITH THIS BC THIS COULD BE ON TEST
+x = "hello",
+print(x)
